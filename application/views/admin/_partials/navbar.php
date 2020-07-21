6a8a2680cd1ae36c096eff1330272f135a1c6043
kiriX1 <mr.yulianto17@gmail.com>
2020-07-21 13:15:24 +0700

    Final version.1.0.0

--- application/views/admin/_partials/navbar.php
+++ application/views/admin/_partials/navbar.php
@@ -57,20 +57,20 @@
         </button>
     
     <a class="navbar-brand ml-3 mr-4" href="<?php echo site_url('admin') ?>">
-    <img src="<?= base_url('assets/img/star.png')?>" width="80px"><?php echo"&nbsp STAR Software"?>
+    <img src="<?= base_url('assets/img/star.png')?>" width="80px"><?php echo"&nbsp PT. STAR Software Indonesia"?>
     </a>
       
 
     <!-- Navbar Search -->
     <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-        <div class="input-group">
+        <!-- <div class="input-group">
             <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
             <div class="input-group-append">
                 <button class="btn btn-light" type="button">
                     <i class="fas fa-search"></i>
                 </button>
             </div>
-        </div>
+        </div> -->
     </form>
 
     <!-- Navbar -->
